b871ce144fde33998ebf6a41c2e38592cc3c6056
Mavriche Raluca <ralucamavriche@gmail.com>
2020-10-08 16:30:14 +0300

    Small changes

---
+++ client/build/precache-manifest.2e67f23ea55eba8e8e9fa32baed673ee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a3a9d74bf415b0747e28086b11b8e1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f954a9d67f04423d1905",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3806f3abba429c381106",
+    "url": "/static/css/main.4aa60ff1.chunk.css"
+  },
+  {
+    "revision": "f954a9d67f04423d1905",
+    "url": "/static/js/2.86997b7e.chunk.js"
+  },
+  {
+    "revision": "b5cecc2895fe0f24d240b53479b28a1c",
+    "url": "/static/js/2.86997b7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3806f3abba429c381106",
+    "url": "/static/js/main.62b5e613.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "15eaf260e865cbc29b4c76594cb698ea",
+    "url": "/static/media/3695234.15eaf260.jpg"
+  }
+]);
